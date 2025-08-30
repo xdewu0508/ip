f43a40360492b820199a2e6cbbe21a9523eebbb2
xdewu0508 <xdewu2005@163.com>
2025-08-30 23:15:26 +0800

    Finish Level-1

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Leo {
     public static void main(String[] args) {
-        String greeting = "____________________\n"
-                + "Hello! I'm Leo."
-                + "What can I do for you?\n"
-                + "____________________\n"
-                + "Bye! Hope to see you soon!";
-        System.out.println(greeting);
+        final String BAR = "____________________";
+
+        System.out.println(BAR);
+        System.out.println("Hello! I'm Leo.");
+        System.out.println("What can I do for you?");
+        System.out.println(BAR);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            if (!sc.hasNextLine()) {
+                break;
+            }
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(line);
+        }
+        sc.close();
+
+        System.out.println(BAR);
+        System.out.println("Bye! Hope to see you soon!");
     }
 }
