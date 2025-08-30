3e87b4e78e2c58a36a7cdea102820dd72e2a02a4
xdewu0508 <xdewu2005@163.com>
2025-08-30 22:23:41 +0800

    Finish Level-0

---
+++ src/main/java/Leo.java
@@ -0,0 +1,10 @@
+public class Leo {
+    public static void main(String[] args) {
+        String greeting = "____________________\n"
+                + "Hello! I'm Leo."
+                + "What can I do for you?\n"
+                + "____________________\n"
+                + "Bye! Hope to see you soon!";
+        System.out.println(greeting);
+    }
+}
