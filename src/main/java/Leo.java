1838bc9fdfacebd5b36cfb94a18ae559758f29b3
xdewu0508 <xdewu2005@163.com>
2026-01-23 15:48:13 +0800

    Level-0

---
+++ src/main/java/Leo.java
@@ -0,0 +1,11 @@
+public class Leo {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
