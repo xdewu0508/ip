9ae5049b4713dbd4fbe2daa92cc051aeaeb38dca
xdewu0508 <xdewu2005@163.com>
2026-01-30 15:19:47 +0800

    Level-1: Echo user input and exit on bye

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,11 +1,25 @@
+//package
+
+import java.util.Scanner;
 public class Leo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = "____________________________________________________________\n" +
-                " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " Hello! I'm Leo\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(greeting);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________\n" +
+                    input +
+                    "\n____________________________________________________________");
+        }
     }
 }
